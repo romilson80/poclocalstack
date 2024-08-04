fcc427d78742b2cce8f53fd2b6b65c04ae3f9ea1
romilson80 <romilson.barros@ibm.com>
2024-08-03 23:16:39 -0300

    Ajustes no Readme

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Este projeto é uma aplicação Java desenvolvida com Spring Boot. A aplicação
 Antes de começar, certifique-se de que você tenha as seguintes ferramentas instaladas:
 
 - [Java 17 ou superior](https://www.oracle.com/java/technologies/javase-jdk17-downloads.html) (ou [OpenJDK](https://openjdk.java.net/))
+- [Git Bash](https://git-scm.com/downloads)
 - [Maven 3.3.2 ou superior](https://maven.apache.org/download.cgi) 
 - [Docker](https://www.docker.com/products/docker-desktop)
 - [Docker Compose](https://docs.docker.com/compose/)
@@ -22,4 +23,11 @@ Clone o repositório do projeto para sua máquina local:
 ```bash
 git clone https://github.com/romilson80/poclocalstack.git
 ```
-Para executar a infra estrutura da aplicação navegue ate 
+Para executar a infraestrutura da aplicação certifique-se que o Docker está em execução. Navegue até 
+o diretorio docker-compose/start-infra.sh e aperte o botão play verde e veja a mágica acontecer.
+
+![start-infra](images/start.png)
+
+A infra da aplicação também poderá ser iniciada atravez de linha de comando do bash como mostrado na imagem abaixo:
+
+![start-infra](images/start2.png)
--- src/main/java/br/com/banzo/poclocalstack/configuration/SqsConfiguration.java
+++ src/main/java/br/com/banzo/poclocalstack/configuration/SqsConfiguration.java
@@ -1,27 +1,27 @@
-//package br.com.banzo.poclocalstack.configuration;
-//
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import software.amazon.awssdk.regions.Region;
-//import software.amazon.awssdk.services.sqs.SqsAsyncClient;
-//
-//import java.net.URI;
-//
-//
-//@Configuration
-//public class SqsConfiguration {
-//
-//    @Value("${sqs.endpoint}")
-//    public String endpoint;
-//
-//    @Bean
-//    public SqsAsyncClient sqsAsyncClient() {
-//        return SqsAsyncClient.builder()
-//                .endpointOverride(URI.create(endpoint))
-//                .region(Region.US_EAST_1)
-//                .build();
-//    }
-//
-//}
+package br.com.banzo.poclocalstack.configuration;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+import java.net.URI;
+
+
+@Configuration
+public class SqsConfiguration {
+
+    @Value("${sqs.endpoint}")
+    public String endpoint;
+
+    @Bean
+    public SqsAsyncClient sqsAsyncClient() {
+        return SqsAsyncClient.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.US_EAST_1)
+                .build();
+    }
+
+}
